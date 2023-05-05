cc1269e57118312ed0b657accdefad71a7578f77
nichol6s <nicholas.jake15@gmail.com>
2023-05-05 09:23:19 -0300

    JavaScript aplicado usando var/let/const

---
+++ js/script.js
@@ -0,0 +1,84 @@
+console.log("Olá Mundo")
+
+//Comentário em linha
+
+        /* Comentario
+            de
+            Blobo*/
+
+        // Declarar uma variável com a palavra reservada var
+
+        // var nr1 = 10;
+        // var nr2 = 5;
+
+        // //Imprimir as variáveis com o objeto console.
+
+        // console.log(nr1);
+        // console.log(nr2);
+
+        // //Realizando as operações básicas com estes valores
+
+        // // + / - / * e %
+
+        // //Adição
+
+        // /* Jeito básico
+        // console.log("Resultado: " + (nr1 + nr2)); */
+        
+        // /*Jeito certinho
+        // console.log("Resultado: " + (parseInt(nr1) + parseInt(nr2))); */
+        
+        // //"Melhor" jeito de fazer
+        // console.log(`Resultado Adição: ${parseInt(nr1) + parseInt(nr2)}`);
+
+        // //SUBTRAÇÃO
+
+        // //console.log("Resultado: " + (nr1 - nr2));
+        // console.log(`Resultado Subtração: ${parseInt(nr1) - parseInt(nr2)}`);
+
+        // //MULTIPLICAÇÃO
+
+        // //console.log("Resultado: " + (nr1 * nr2));
+        // console.log(`Resultado Multiplicação: ${parseInt(nr1) * parseInt(nr2)}`);
+
+        // //DIVISÃO
+
+        // //console.log("Resultado: " + (nr1 / nr2))
+        // console.log(`Resultado Divisão: ${parseInt(nr1) / parseInt(nr2)}`);
+
+        // //OBTENHA O RESTO DA DIVISÃO
+
+        // //console.log("Resultado: " + (nr1 % nr2))
+        // console.log(`Resultado do resto da divisão: ${parseInt(nr1) % parseInt(nr2)}`);
+
+        //DESCOBRINDO SE UM NÚMERO É PAR OU IMPAR
+        //UTILIZANDO OPERADOR TERNÁRIO ( ? )
+
+        // var nr1 = "10";
+        // var nr2 = "5";
+        // var resultado;
+
+        // resultado = !(parseInt(nr1) % parseInt(nr1)) ? "Par" : "Impar";
+        // console.log(`O número é impar: ${resultado}`)
+
+        //DIFERENÇA ENTRE VAR/LET/CONST
+
+        //var nome1 = "Beatriz";
+        //let nome2 = "Matheus";
+
+        //REGRAS DO CONST
+        //1 REGRA = Inicializar na declaração
+        //const nome3 = "Violante";
+        
+        //2 REGRA = Não pode ser alterado durante o fluxo do programa
+        //nome3 = "Jonathan";
+
+        let nome = "Beatriz";
+
+        if(nome != ""){
+            let nome = "Robson"
+        }
+
+        console.log(nome);
+
+        
